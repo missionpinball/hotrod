7bdd74894dc2cfb44d058404c4514b4d987a3c82
Jan Kantert <jan-mpf@kantert.net>
2017-04-14 19:26:42 +0200

    2 and 3 ball mb start with test

--- tests/test_Multiball.py
+++ tests/test_Multiball.py
@@ -4,7 +4,7 @@ from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
 
 class TestMultiball(MpfMachineTestCase, MpfGameTestCase):
 
-    def test_lock_on_skillshot(self):
+    def test_lock_on_skillshot_and_three_ball(self):
         # start game
         self.hit_and_release_switch("cs_start")
         self.advance_time_and_run(5)
@@ -26,7 +26,62 @@ class TestMultiball(MpfMachineTestCase, MpfGameTestCase):
         self.assertModeNotRunning("skill_shot")
         self.assertModeRunning("lock")
 
-    def test_lock_on_drop_targets(self):
+        # should do nothing
+        self.hit_and_release_switch("pfs_left_eject")
+
+        self.assertModeNotRunning("2ball")
+        self.assertModeNotRunning("3ball")
+
+        self.hit_switch_and_run("pfs_left_saucer", 5)
+        self.assertModeRunning("lock")
+        self.assertModeNotRunning("lock_lit")
+        self.assertBallsOnPlayfield(1)
+        self.assertBallsInPlay(1)
+        self.hit_switch_and_run("pfs_right_saucer", 5)
+        self.assertModeRunning("lock")
+        self.assertModeRunning("lock_lit")
+        self.assertBallsOnPlayfield(1)
+        self.assertBallsInPlay(1)
+        self.hit_switch_and_run("pfs_left_saucer", 5)
+        self.assertModeRunning("lock")
+        self.assertModeRunning("lock_lit")
+        self.assertBallsOnPlayfield(0)
+        self.assertBallsInPlay(1)
+        self.release_switch_and_run("pfs_shooter_lane", 4)
+        self.assertBallsOnPlayfield(1)
+        self.assertEqual(1, self.machine.ball_devices.bd_left_saucer.balls)
+        self.assertEqual(0, self.machine.ball_devices.bd_right_saucer.balls)
+
+        self.hit_switch_and_run("pfs_right_saucer", 5)
+        self.assertBallsOnPlayfield(0)
+        self.assertBallsInPlay(1)
+        self.assertEqual(1, self.machine.ball_devices.bd_left_saucer.balls)
+        self.assertEqual(1, self.machine.ball_devices.bd_right_saucer.balls)
+
+        self.assertFalse(self.machine.multiballs.mb_2ball.balls_added_live)
+        self.assertFalse(self.machine.multiballs.mb_3ball.balls_added_live)
+
+        self.assertModeNotRunning("2ball")
+        self.assertModeRunning("3ball")
+
+        self.release_switch_and_run("pfs_shooter_lane", 4)
+        self.assertBallsOnPlayfield(1)
+
+        self.assertTrue(self.machine.multiballs.mb_3ball.balls_added_live)
+        self.assertTrue(self.machine.multiballs.mb_3ball.shoot_again)
+        self.advance_time_and_run(3)
+
+        self.assertEqual(0, self.machine.ball_devices.bd_left_saucer.balls)
+        self.assertEqual(0, self.machine.ball_devices.bd_right_saucer.balls)
+        self.assertBallsInPlay(3)
+        self.assertBallsOnPlayfield(3)
+
+        self.drain_ball()
+        self.advance_time_and_run(3)
+        self.assertBallsInPlay(3)
+        self.assertBallsOnPlayfield(2)
+
+    def test_lock_on_drop_targets_and_two_ball(self):
         # start game
         self.hit_and_release_switch("cs_start")
         self.advance_time_and_run(5)
@@ -54,6 +109,12 @@ class TestMultiball(MpfMachineTestCase, MpfGameTestCase):
 
         self.assertModeRunning("lock")
 
+        # should do nothing
+        self.hit_and_release_switch("pfs_left_eject")
+
+        self.assertModeNotRunning("2ball")
+        self.assertModeNotRunning("3ball")
+
         self.hit_switch_and_run("pfs_left_saucer", 5)
         self.assertModeRunning("lock")
         self.assertModeNotRunning("lock_lit")
@@ -69,12 +130,24 @@ class TestMultiball(MpfMachineTestCase, MpfGameTestCase):
         self.assertModeRunning("lock_lit")
         self.assertBallsOnPlayfield(0)
         self.assertBallsInPlay(1)
-        self.release_switch_and_run("pfs_shooter_lane", 11)
+        self.release_switch_and_run("pfs_shooter_lane", 4)
         self.assertBallsOnPlayfield(1)
         self.assertEqual(1, self.machine.ball_devices.bd_left_saucer.balls)
+        self.assertFalse(self.machine.multiballs.mb_2ball.balls_added_live)
 
         self.hit_and_release_switch("pfs_left_eject")
-        self.advance_time_and_run(11)
+        self.advance_time_and_run(3)
         self.assertEqual(0, self.machine.ball_devices.bd_left_saucer.balls)
         self.assertBallsInPlay(2)
         self.assertBallsOnPlayfield(2)
+        self.assertModeRunning("2ball")
+        self.assertModeNotRunning("3ball")
+
+        self.assertTrue(self.machine.multiballs.mb_2ball.balls_added_live)
+        self.assertTrue(self.machine.multiballs.mb_2ball.shoot_again)
+
+        self.drain_ball()
+        self.advance_time_and_run(3)
+        self.assertBallsInPlay(2)
+        self.assertBallsOnPlayfield(1)
+
