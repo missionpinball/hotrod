d4dd7d27ae32a2314446cd3c308680921eb051b3
Jeremy Wilson <jwilson@syslog.com>
2018-03-16 14:41:56 -0400

    Added support for flashing 100k lamp up to 900,000

--- scriptlets/100k.py
+++ scriptlets/100k.py
@@ -6,14 +6,20 @@ class SixDigits(Scriptlet):
         self.log = logging.getLogger('Watch for 100k')
         self.log.setLevel('DEBUG')
         self.machine.events.add_handler("player_score",self.check_100k)
-    
+
     def check_100k(self, **kwargs):
         score = kwargs.get("value")
+        num_flash = int(score / 100000)
         player = kwargs.get("player_num")
-        
-        if score >= 100000:
+
+        if num_flash > 9:
+            num_flash == 9
+
+        if score < 100000:
+            self.machine.lights['bbl_player_' + str(player) + '_100k'].off()
+
+        if score >= 100000 and score < 200000:
             self.log.debug('100k Lamp ON for Player ' + str(player))
             self.machine.lights['bbl_player_' + str(player) + '_100k'].on()
-        else:
-            #self.log.debug('100k Lamp OFF for Player ' + str(player))
-            self.machine.lights['bbl_player_' + str(player) + '_100k'].off()
+        if score >= 200000:
+            self.machine.shows['flash_100k_' + str(num_flash)].play(show_tokens=dict(light='bbl_player_' + str(player) + '_100k'))
