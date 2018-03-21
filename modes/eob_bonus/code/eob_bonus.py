28507c8ac24b89df42459fab0313a2b0826b138f
Jeremy Wilson <jwilson@syslog.com>
2018-03-21 11:24:30 -0400

    Trying to get bonus countdown to work

--- modes/eob_bonus/code/eob_bonus.py
+++ modes/eob_bonus/code/eob_bonus.py
@@ -1,12 +1,13 @@
 from mpf.core.mode import Mode
 from mpf.core.delays import DelayManager
+from mpf.core.utility_functions import Util
 
 class Count(Mode):
 
     def mode_start(self, **kwargs):
-        self.bonus_value = 0
-        self.count_down = self.player['lb_bonus']
-        self.machine.events.post("bonus_code_starting")
+        self.bonus_value = 1000
+        self.count_down = self.machine.counters.lb_bonus.value
+        self.machine.events.post("bonus_code_starting_value_" + str(self.count_down))
         self.prepare_bonus()
 
     def prepare_bonus(self):
@@ -15,14 +16,19 @@ class Count(Mode):
             self.stop()
             return
 
-        self.bonus_value = (self.config['scoring']['bonus_value']['score'])
         self.check_score_reels()
 
     def check_score_reels(self, **kwargs):
-        ready_future = Util.ensure_future(self.machine.score_reel_groups.your_group.wait_for_ready(), loop=self.machine.clock.loop)
+        self.machine.events.post("bonus_code_check_score_reels_valid")
+        if self.player['player_number'] == 1:
+            ready_future = Util.ensure_future(self.machine.score_reel_groups.player1.wait_for_ready(), loop=self.machine.clock.loop)
+        else:
+            ready_future = Util.ensure_future(self.machine.score_reel_groups.player2.wait_for_ready(), loop=self.machine.clock.loop)
+
         ready_future.add_done_callback(self.bonus_step)
 
-    def bonus_step(self, **kwargs):
+    def bonus_step(self, future=None, **kwargs):
+        self.machine.events.post("bonus_code_step")
         if self.count_down > 0:
             self.delay.add(ms=200, callback=self.do_bonus_step)
         else:
@@ -62,7 +68,7 @@ class Count(Mode):
             self.machine.shows['flash'].play(show_tokens=dict(light='pfl_bonus_1000'), speed=10.0, loops=4)
             self.machine.lights.pfl_bonus_1000.off()
 
-        self.machine.scoring.add(self.bonus_value)
+        self.player.score += self.bonus_value
         self.count_down -= self.bonus_value
 
     def bonus_done(self):
