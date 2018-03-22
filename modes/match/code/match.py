983ea5ab5ac72514a718673085e8726f6615c609
Jeremy Wilson <jwilson@syslog.com>
2018-03-22 12:50:04 -0400

    trouble with match mode

--- modes/match/code/match.py
+++ modes/match/code/match.py
@@ -5,50 +5,42 @@ from random import *
 class Match(Mode):
 
     def mode_start(self, **kwargs):
-        player_1_match = False
-        player_2_match = False
+        self.player_1_match = False
+        self.player_2_match = False
   
-        match_score_player_1 = 0
-        match_score_player_2 = 0
+        self.match_numbers = ['00', '10', '20', '30', '40', '50', '60', '70', '80', '90']
+        shuffle(self.match_numbers)
+        self.match = self.match_numbers[0]
+        self.match_light = "bbl_match_" + self.match
         
-        match_numbers = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
-        shuffle(match_numbers)
-        match = int(match_numbers[0])
-        
-        player_1_score = self.machine.get_machine_var('player1_score')
+        # Determine last two digits of player 1 score for match
+        self.player_1_score = self.machine.get_machine_var('player1_score')
+        self.match_score_player_1 = '{:02}'.format(int(str(int(str(self.player_1_score)[-2:]))[-2:1]) * 10)
 
-        if self.machine.game.num_players == 2:
-            player_2_score = self.machine.get_machine_var('player2_score')
-        else:
-            player_2_score = 0
+        if self.match == self.match_score_player_1:
+            self.player_1_match = True
+            self.player_match = True
 
-        if player_1_score > 9:
-            match_score_player_1 = int(str(int(str(player_1_score)[-2:]))[-2:1]) * 10
-        if player_2_score > 9:
-            match_score_player_2 = int(str(int(str(player_2_score)[-2:]))[-2:1]) * 10
-        
-        if match == match_score_player_1:
-            player_1_match = True
-            
+        # If a 2 player game, also determine last two digits for player 2
         if self.machine.game.num_players == 2:
-            if match == match_score_player_2:
-                player_2_match = True
-        
+            self.player_2_score = self.machine.get_machine_var('player2_score')
+            self.match_score_player_2 = '{:02}'.format(int(str(int(str(self.player_2_score)[-2:]))[-2:1]) * 10)
+
+            if self.match == self.match_score_player_2:
+                self.player_2_match = True
+                self.player_match = True
+
+        self.machine.events.post('match_' + self.match)
         self.machine.lights['bbl_match'].on()
+        self.machine.lights[self.match_light ].on()
 
-        if match == 0:
-            self.machine.lights['bbl_match_00'].on()
-        else:
-            self.machine.lights['bbl_match_' + str(match)].on()
-            
-        self.machine.events.post('match_' + str(match))
-
-        if player_1_match:
+        if self.player_1_match:
             self.machine.shows['match_knocker'].play(loops=0)
             self.machine.events.post('match_player_1')
-        if player_2_match:
+        if self.player_2_match:
             self.machine.shows['match_knocker'].play(loops=0)
             self.machine.events.post('match_player_2')
 
+        self.machine.events.post('match_code_ended')
         self.stop()
-        
+           
