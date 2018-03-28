b5b219fbdbec8ee4c64e10cc07dcb9b200f22457
Jan Kantert <jan-mpf@kantert.net>
2018-03-28 22:44:05 +0200

    convert match to AsyncMode

--- modes/match/code/match.py
+++ modes/match/code/match.py
@@ -1,11 +1,13 @@
-from mpf.core.mode import Mode
-from mpf.core.delays import DelayManager
-from time import sleep
+import asyncio
+
+from mpf.core.async_mode import AsyncMode
 from random import *
 
-class Match(Mode):
 
-    def mode_start(self, **kwargs):
+class Match(AsyncMode):
+
+    @asyncio.coroutine
+    def _run(self):
         self.player_1_match = False
         self.player_2_match = False
         self.player_match = False
@@ -22,7 +24,7 @@ class Match(Mode):
 
         # Determine last two digits of player 1 score for match
         player_1_score = self.machine.game.player_list[0].score
-            
+
         self.log.info('Player 1 score: ' + str(player_1_score))
         match_score_player_1 = '{:02}'.format(int(str(int(str(player_1_score)[-2:]))[-2:1]) * 10)
         self.log.info('Player 1 Match Score: ' + str(match_score_player_1))
@@ -34,7 +36,7 @@ class Match(Mode):
         # If a 2 player game, also determine last two digits for player 2
         if self.machine.game.num_players == 2:
             player_2_score = self.machine.game.player_list[1].score
-                
+
             match_score_player_2 = '{:02}'.format(int(str(int(str(player_2_score)[-2:]))[-2:1]) * 10)
 
             if self.match == match_score_player_2:
@@ -42,19 +44,20 @@ class Match(Mode):
                 self.player_match = True
 
         self.machine.events.post('match_' + self.match)
-        
+
         match_show = randint(1,3)
         self.log.info('Match Show: ' + str(match_show))
 
         #if match_show == 1:
-        self.match_show_circle_right()
+        yield from self.match_show_circle_right()
         #if match_show == 2:
         #    self.match_show_circle_left()
         #if match_show == 3:
         #    self.match_show_classic()
-        
-        self.match_done()
 
+        yield from self.match_done()
+
+    @asyncio.coroutine
     def match_show_classic(self):
         # Just turn on lamps
         self.machine.events.post('match_show_classic')
@@ -62,12 +65,16 @@ class Match(Mode):
         self.machine.lights[self.match_light].on(key="match")
         self.match_flash()
 
+    @asyncio.coroutine
     def match_show_circle_right(self):
         # Rotating Match lamps
-        self.machine.events.add_handler('match_show_circle_right_complete', self.match_show_circle_right_complete)
         self.machine.events.post('match_show_circle_right')
         self.machine.shows['match_circle_right'].play(events_when_completed=["match_show_circle_right_complete"],loops=0)
+        # wait for show to finish
+        yield from self.machine.events.wait_for_event("match_show_circle_right_complete")
+        yield from self.match_show_circle_right_complete()
 
+    @asyncio.coroutine
     def match_show_circle_right_complete(self, **kwargs):
         match_numbers_circle = ['00','20','40','60','80','90','70','50','30','10']
 
@@ -79,23 +86,22 @@ class Match(Mode):
             self.machine.lights[previous_match_light_circle].off(key="match")
             self.machine.lights[match_light_circle].on(key="match")
             yield from asyncio.sleep(0.1)
-            
+
             if lamp == self.match:
                 break
-                
+
             previous_match_light_circle = match_light_circle
 
-        self.match_flash()
+        yield from self.match_flash()
 
+    @asyncio.coroutine
     def match_flash(self):
         # Flash Match number if matched
-        self.machine.events.add_handler('match_flash_complete', self.match_done)
-
         if self.player_match:
-            self.machine.shows['flash_match'].play(show_tokens=dict(light=match_light_circle))
-        else:
-            self.match_done()
-        
+            self.machine.shows['flash_match'].play(show_tokens=dict(light="match_light_circle"))
+            yield from self.machine.events.wait_for_event("match_flash_complete")
+
+    @asyncio.coroutine
     def match_done(self):
         self.machine.lights[self.match_light].on(key="match")
 
@@ -108,4 +114,4 @@ class Match(Mode):
             self.machine.events.post('match_player_2')
 
         self.machine.events.post('match_code_ended')
-           
+
